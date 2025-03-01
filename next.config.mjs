7ba99986285df03cf16601d4dc096e58c22b5b6d
mrv777 <mrv777@users.noreply.github.com>
2025-03-01 12:33:23 -0600

    Remove unoptimized image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // For static exports
-  },
 };
 
 export default nextConfig; 
