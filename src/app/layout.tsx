cff1a19cce47f3f6b013bb2878cc37016ab836b8
mrv777 <mrv777@users.noreply.github.com>
2025-03-01 12:22:46 -0600

    fix: create favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'OMB Gallery',
   description: 'A zoomable gallery of OMB images',
+  icons: {
+    icon: [
+      { url: '/favicon-16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
+    ],
+  },
 };
 
 export default function RootLayout({
